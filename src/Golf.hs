50fc251ee480fb035012dad768fe3a33ddd5766e
Mon Zafra <mz@codeia.ph>
2019-03-11 17:37:26 +0800

    finished week4

--- src/Golf.hs
+++ src/Golf.hs
@@ -9,7 +9,7 @@ import Data.List (group, sort, transpose)
 -- decreases each index by 1 then calls the (!!) operator on the list with the
 -- indices. The outer comprehension just counts from 1 to the length of the list.
 skips :: [a] -> [[a]]
-skips l = [[l !! (i - 1) | i <- [n, 2*n..length l]] | n <- [1..length l]]
+skips l = [[l !! pred i | i <- [n, 2*n..length l]] | n <- [1..length l]]
 
 
 -- Ex. 2
@@ -23,12 +23,12 @@ localMaxima l = [b | (a, b, c) <- zip3 l (drop 1 l) (drop 2 l), b > a && b > c]
 
 -- Ex. 3
 -- Make a frequency list by sorting and grouping the original list (append [0..9]
--- to ensure that each digit has a value in the resulting frequency list).  Turn
--- the grouped digits into a number by counting the elements and subtracting 1 to
--- compensate for the filler. Then convert it into a string by mapping it to a
+-- to ensure that each digit is represented in the resulting frequency list).
+-- Turn the grouped digits into a number by counting the elements and subtracting
+-- 1 to compensate for the filler. Convert it into a string by mapping it to a
 -- string of '*' with the corresponding lengths. Right-justify the strings to the
 -- highest frequency in the list so that the list can be transposed.  Transpose,
--- then append the divider and labels, then join the list into a string.
+-- append the divider and labels, then implode the lines into a string.
 histogram :: [Integer] -> String
 histogram l
     =  unlines
---
+++ src/HigherOrder.hs
@@ -0,0 +1,77 @@
+-- Week 4
+module HigherOrder where
+
+import Data.List (transpose)
+
+-- Ex. 1
+fun1' :: [Integer] -> Integer
+fun1' = product . map (-2 +) . filter even
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (> 1) . iterate next
+  where
+    next n
+        | even n = n `div` 2
+        | otherwise = 3*n + 1
+
+
+-- Ex. 2
+data Tree a
+    = Leaf
+    | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr add Leaf
+  where
+    add a Leaf = Node 0 Leaf a Leaf
+    add a (Node 0 _ p _) = Node 1 (add a Leaf) p Leaf
+    add a (Node 1 left p Leaf) = Node 1 left p (add a Leaf)
+    add a node@(Node ht left p right)
+        | slope node /= 0 || slope left < slope right =
+            Node ht (add a left) p right
+        | otherwise =
+            let right' = add a right
+            in  Node (height right' + 1) left p right'
+    height Leaf = -1
+    height (Node ht _ _ _) = ht
+    slope Leaf = undefined
+    slope (Node _ left _ right) = height left - height right
+
+visualize :: Tree Char -> String
+visualize tree = unlines $ transpose $ padRight columns
+  where
+    columns = draw tree
+    len = maximum $ map length columns
+    padRight = map $ take len . (++ repeat ' ')
+    draw Leaf = ["_"]
+    draw (Node 0 _ a _) = [[a]]
+    draw (Node _ left a right) = indent left ++ [a] : indent right
+    indent = map (" " ++) . draw
+
+trace :: Char -> Char -> IO ()
+trace from to = putStr $ unlines $ do
+    c <- [from..to]
+    return $ visualize $ foldTree [c, pred c..'A']
+
+
+-- Ex. 3
+xor :: [Bool] -> Bool
+xor = foldr (/=) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr ((:) . f) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f = flip $ foldr (\b k a -> k $ f a b) id
+
+
+-- Ex. 4
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n
+    = (2 :)
+    . map ((+ 1) . (* 2))
+    . filter (not . (`elem` marked))
+    $ [1..n]
+  where
+    marked = filter (<= n) [i + j + 2*i*j | j <- [1..n], i <- [1..j]]
